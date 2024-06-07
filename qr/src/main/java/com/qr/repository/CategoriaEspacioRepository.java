eb1b6e252be0b293e8505c52425f8f08036c6b6d
serrb <162188554+serrb@users.noreply.github.com>
2024-06-07 19:40:11 +0200

    Delete qr directory

--- qr/src/main/java/com/qr/repository/CategoriaEspacioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.qr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.qr.model.CategoriaEspacio;
-
-public interface CategoriaEspacioRepository extends JpaRepository<CategoriaEspacio, Long> {
-
-}
--- qr/src/main/java/com/qr/repository/CategoriaObjetoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.qr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.qr.model.Categoria;
-
-public interface CategoriaObjetoRepository extends JpaRepository<Categoria, Long>{
-
-}
