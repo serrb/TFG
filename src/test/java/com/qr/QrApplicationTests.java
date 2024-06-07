586822c49c2036a958ed9f2636bf1fa42e8a5c3c
serrb <162188554+serrb@users.noreply.github.com>
2024-06-07 20:37:37 +0200

    Update QrApplicationTests.java

--- src/test/java/com/qr/QrApplicationTests.java
+++ src/test/java/com/qr/QrApplicationTests.java
@@ -3,7 +3,7 @@ package com.qr;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class QrApplicationTests {
 
 	@Test
